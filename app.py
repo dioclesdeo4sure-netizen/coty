b3782566f480a146cee50e4d0f23284efb252cdd
dioclesdeo4sure-netizen <dioclesdeo4sure@gmail.com>
2026-01-24 12:47:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def get_db_connection():
     return psycopg2.connect(
         database=result.path[1:],
         user=result.username,
-        password=result.password,
+       password=result.password,
         host=result.hostname,
         port=result.port
     )
